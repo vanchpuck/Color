c9f6af48fdd6f7a0e584bd29050ff6265c24f1ac
vanchpuck <vanchpuck@mail.ru>
2014-05-27 01:35:58 +0400

    Добавлены новые активити (пока на время отладки).

---
+++ ColorCombination/src/com/jonnygold/colors/EntryActivity.java
@@ -0,0 +1,33 @@
+package com.jonnygold.colors;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class EntryActivity extends Activity {
+
+	@Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_entry);
+	}
+	
+	public void createPalette(View view) {
+		Intent intent = new Intent(this, MainActivity.class);
+		startActivity(intent);
+	}
+	
+	public void getFromGallery(View view) {
+		Intent intent = new Intent(Intent.ACTION_PICK);
+		intent.setType("image/*");
+		startActivityForResult(intent, 0);
+	}
+	
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		Intent intent = new Intent(this, ImageActivity.class);
+		startActivity(intent);
+	}
+	
+}
---
+++ ColorCombination/src/com/jonnygold/colors/ImageActivity.java
@@ -0,0 +1,14 @@
+package com.jonnygold.colors;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ImageActivity extends Activity {
+
+	@Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_image);
+	}
+		
+}
